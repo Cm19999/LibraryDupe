46c2f59dbae69905e7d3eafdedc94982df56c8ce
callum <cmarsden4163@googlemail.com>
2021-10-18 14:02:14 +0100

    finished empty test

--- src/test/java/company/testPaper.java
+++ src/test/java/company/testPaper.java
@@ -1,5 +1,15 @@
 package company;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class testPaper {
+
+    @Test
+    public void testUse(){
+        Paperback a = new Paperback("usedd","kirby","7");
+        assertEquals("Incorrect output","Flicks through pages...",a.use());
+    }
 
 }
